8a4e681f773796be1b564a834d0afd95e637e7c1
Moxh <moxhsharma878@gmail.com>
2025-03-26 00:35:15 +0530

    we print code

--- CWH_05_TakingInpu.java
+++ CWH_05_TakingInpu.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class CWH_05_TakingInpu {
     public static void main(String[] args) {
         System.out.println("Calculating Percentage Marks of Student");
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);// making new class 
         System.out.println("Enter Marks of Math");
         float a= sc.nextFloat();
-        System.out.println("Enter Marks in English");
+        System.out.println("Enter Marks in English ");
         float b=sc.nextFloat();
         System.out.println("Enter Marks in Biology");
         float c=sc.nextFloat();
@@ -20,7 +20,7 @@ public class CWH_05_TakingInpu {
         System.out.println("Percentage of Marks :");
         System.out.print(percentage);
         System.out.print("%");
-        
+        System.out.println("this is your code");
     }
     
 }
